eddec0676f2a6f12a26cc07334e638d95325ba02
AnnaKachkaeva <63503721+AnnaKachkaeva@users.noreply.github.com>
2024-05-19 20:48:39 +0300

    Add files via upload

--- swiftui/swiftui/ContentView.swift
+++ swiftui/swiftui/ContentView.swift
@@ -15,55 +15,117 @@ extension AnyTransition {
 
 
 struct ContentView: View {
-    @Namespace private var animation
-    @State private var isExpanded: Bool = false
+    @Namespace var animation
+    @State var showDetail = false
 
     var body: some View {
-        if isExpanded {
-            ZStack(alignment: .topLeading) {
-                Color.cyan
-                    .cornerRadius(25)
-                    .matchedGeometryEffect(id: "button", in: animation)
-                    .frame(
-                        width: isExpanded ? 350 : 100,
-                        height: isExpanded ? 450 : 50
-                    )
-                Button(action: {
-                    withAnimation(.spring) {
-                        isExpanded.toggle()
+        ZStack {
+            if (!showDetail) {
+                HStack {
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        Button("open me") {
+                            withAnimation {
+                                showDetail.toggle()
+                            }
+                        }
+                        .matchedGeometryEffect(id: "button", in: animation)
+                        .buttonStyle(.borderedProminent)
+                        .padding(.trailing)
                     }
-                }) {
-                    HStack {
-                        Text("<- Back")
-                            .font(.title3.bold())
-                    }
-                    .padding()
-                    .foregroundStyle(.white)
-                    .matchedGeometryEffect(id: "Text", in: animation, properties: .position, anchor: .leading)
                 }
+            } else {
+                DetailView(showDetail: $showDetail, animation: _animation)
             }
-        } else {
-            Button(action: {
-                withAnimation(.spring) {
-                    isExpanded.toggle()
-                }
-            }) {
-                Color.cyan
-                    .cornerRadius(15)
-                    .matchedGeometryEffect(id: "button", in: animation)
-                    .frame(
-                        width: !isExpanded ? 100 : 350,
-                        height: !isExpanded ? 50 : 450
-                    )
-                    .overlay(
-                        Text("Open")
-                            .font(.title3.bold())
-                            .foregroundStyle(.white)
-                            .matchedGeometryEffect(id: "Text", in: animation, properties: .position, anchor: .leading)
-                    )
-                    .padding()
+        }
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 4)) {
+                showDetail.toggle()
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: isExpanded ? .center : .bottomTrailing)
         }
     }
 }
+
+
+
+struct DetailView: View {
+    @Binding var showDetail: Bool
+    @Namespace var animation
+
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(.blue)
+            .frame(
+                width: showDetail ? 350 : 100,
+                height: showDetail ? 450 : 50
+            )
+            .overlay(
+                alignment: .topLeading
+            ) {
+                Button("<- back") {
+                    print("tapped")
+                    showDetail.toggle()
+                }
+                .foregroundColor(.white)
+                .padding(.all)
+            }
+            .matchedGeometryEffect(id: "button", in: animation)
+    }
+}
+
+
+//struct ContentView: View {
+//    @Namespace private var animation
+//    @State private var isExpanded: Bool = false
+//
+//    var body: some View {
+//        if isExpanded {
+//            ZStack(alignment: .topLeading) {
+//                Color.blue
+//                    .cornerRadius(25)
+//                    .matchedGeometryEffect(id: "button", in: animation)
+//                    .frame(
+//                        width: isExpanded ? 350 : 100,
+//                        height: isExpanded ? 450 : 50
+//                    )
+//                Button(action: {
+//                    withAnimation(.spring) {
+//                        isExpanded.toggle()
+//                    }
+//                }) {
+//                    HStack {
+//                        Image(systemName: "arrowshape.backward.fill")
+//                        Text("Back")
+//                            .font(.title3.bold())
+//                    }
+//                    .padding()
+//                    .foregroundStyle(.white)
+//                    .matchedGeometryEffect(id: "Text", in: animation, properties: .position, anchor: .leading)
+//                }
+//            }
+//        } else {
+//            Button(action: {
+//                withAnimation(.spring) {
+//                    isExpanded.toggle()
+//                }
+//            }) {
+//                Color.blue
+//                    .cornerRadius(15)
+//                    .matchedGeometryEffect(id: "button", in: animation)
+//                    .frame(
+//                        width: !isExpanded ? 100 : 350,
+//                        height: !isExpanded ? 50 : 450
+//                    )
+//                    .overlay(
+//                        Text("Open")
+//                            .font(.title3.bold())
+//                            .foregroundStyle(.white)
+//                            .matchedGeometryEffect(id: "Text", in: animation, properties: .position, anchor: .leading)
+//                    )
+//                    .padding()
+//            }
+//            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: isExpanded ? .center : .bottomTrailing)
+//        }
+//    }
+//}
